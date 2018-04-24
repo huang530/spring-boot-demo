70ca7b1017dac54e17327c9fd886d0e6bba3f1c1
huangxiaogang <978341496@qq.com>
2018-04-24 10:43:00 +0800

    添加测试类

--- src/test/java/com/example/demo/HelloTests.java
+++ src/test/java/com/example/demo/HelloTests.java
@@ -17,12 +17,12 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.awt.PageAttributes.MediaType;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
